c268da1cc49d67333a7fe1dcf5c3f44dd1bff0c0
Marc Blais <marcantblais@gmail.com>
2024-02-10 18:52:27 -0500

    new endpoints

--- src/app/utilities/utilities.ts
+++ src/app/utilities/utilities.ts
@@ -1,4 +1,4 @@
-const url = "http://localhost:8080"
+const url = "https://scrabble.marchome.xyz/api"
 
 export async function postRequest(body: string, path: string): Promise<any> {
     return await fetch(url + path, {
